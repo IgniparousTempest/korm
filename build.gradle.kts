fb9296e7859a1082b02a43a551f79b42e4b28470
IgniparousTempest <cr.pitcher@gmail.com>
2018-10-01 22:01:25 +0200

    WIP coverage gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,4 +34,22 @@ tasks {
     "test"(Test::class) {
         useJUnitPlatform()
     }
+
+    // Task with name 'codeCoverageReport' not found in root project ''.
+    "codeCoverageReport"(JacocoReport::class) {
+        executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
+
+        subprojects.onEach {
+            sourceSets(it.sourceSets.main)
+        }
+
+        reports {
+            xml.isEnabled = true
+            xml.destination = File("$buildDir/reports/jacoco/report.xml")
+            html.isEnabled = false
+            csv.isEnabled = false
+        }
+
+        dependsOn("test")
+    }
 }
